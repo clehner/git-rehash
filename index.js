d574407ec1d14b95d4ce3212fbccc434a34fb34d
Charles Lehner <cel@celehner.com>
2016-02-11 11:47:24 -0500

    WIP - Try to get stream reordering working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var crypto = require('crypto')
 var buffered = require('pull-buffered')
 var pull = require('pull-stream')
+var cat = require('pull-cat')
+var pushable = require('pull-pushable')
 
 function createHash(type, onEnd) {
   function hasher(read) {
@@ -9,7 +11,7 @@ function createHash(type, onEnd) {
         if (end === true) hasher.digest = hash.digest()
         else if (!end) hash.update(data)
         cb(end, data)
-        if (end && onEnd) onEnd(end === true ? null : end)
+        if (end && onEnd) onEnd(end === true ? null : end, hasher.digest)
       })
     }
   }
@@ -18,78 +20,154 @@ function createHash(type, onEnd) {
   return hasher
 }
 
-function createGitObjectHash(objectType, objectLength) {
-  var hasher = createHash('sha1')
+function createGitObjectHash(objectType, objectLength, onEnd) {
+  var hasher = createHash('sha1', onEnd)
   hasher.hash.update(objectType + ' ' + objectLength + '\0')
   return hasher
 }
 
-function passthrough(onEnd) {
-  return function (read) {
-    return function (abort, cb) {
-      read(abort, function (end, data) {
-        if (end && onEnd) onEnd(end)
-        cb(end, data)
-      })
-    }
-  }
-}
-
 function rewriteObjectsFromGit(algorithm, lookup) {
   var hashCache = {}
+  var waitingForHash = {}
+  var outQueue = pushable(function (err) {
+    console.error('out queue done', err)
+  })
 
   return function (readObject) {
     var ended
-    return function (abort, cb) {
-      if (ended) return cb(ended)
-      readObject(abort, function (end, obj) {
-        if (ended = end) return cb(ended)
-        var hasherIn = createGitObjectHash(obj.type, obj.length)
+    var readyToResolve
 
+    return cat([
+      readOut,
+      outQueue
+    ])
+
+    function readOut(abort, cb) {
+      if (ended) return cb(ended)
+      readObject(abort, function next(end, obj) {
+        if (end === true) readyToResolve = true
+        if (ended = end) return cb(ended)
+        var hasherIn = createGitObjectHash(obj.type, obj.length, inDone)
+        var hasherOut = createHash(algorithm, outDone)
+        var queue = false
+
+        console.error('object', obj.type)
+
+        // Pass through blobs. Commits, tags, and trees don't call back until
+        // they are finished.
         switch (obj.type) {
           case 'blob':
-            rewrite = passthrough()
-            break
+            return cb(null, {
+              type: obj.type,
+              length: obj.length,
+              read: pull(
+                obj.read,
+                hasherIn,
+                hasherOut
+              )
+            })
           case 'commit':
           case 'tag':
             rewrite = rewriteCommitOrTagFromGit(hasherIn)
             break
           case 'tree':
-            rewrite = rewriteTreeFromGit()
+            rewrite = rewriteTreeFromGit(hasherIn)
             break
           default:
             return cb(new Error('Unknown object type ' + obj.type))
         }
 
-        var hasherOut = createHash(algorithm, next)
+        // console.error('pulling')
+        queue = true
+        pull(
+          obj.read,
+          hasherIn,
+          rewrite,
+          hasherOut,
+          pull.collect(function (err, bufs) {
+            // console.error('collected', err)
+            if (err) return outQueue.end(err)
+            if (bufs.length == 0)
+              throw new Error('no bufs ' + err + ' ' +
+                hasherIn.digest.toString('hex') + ' ' + hasherOut.digest)
+            outQueue.push({
+              type: obj.type,
+              length: obj.length,
+              read: pull.values(bufs)
+            })
+          })
+        )
 
-        cb(null, {
-          type: obj.type,
-          length: obj.length,
-          read: pull(
-            obj.read,
-            hasherIn,
-            rewrite,
-            hasherOut
-          )
-        })
-
-        function next(err) {
+        function inDone(err) {
+          // console.error('in done', err)
           if (err) return cb(err)
-          // console.error('hashed', hasherIn.digest, 'to', hasherOut.digest)
-          hashCache[hasherIn.digest] = hasherOut.digest
+          var gitHash = hasherIn.digest.toString('hex')
+          // console.error('git hash', gitHash)
+          // Mark the git hash as a placeholder so we know that we have this
+          // object but it is waiting for some other objects
+          if (!(gitHash in hashCache))
+            hashCache[gitHash] = null
+          if (queue)
+            readObject(null, next)
+        }
+
+        function outDone(err, digest) {
+          if (err) console.error('out done err', err)
+          if (err) return cb(err)
+          var gitHash = hasherIn.digest.toString('hex')
+          var outHash = digest
+          // console.error('out done', gitHash, '->', outHash)
+          if (gitHash == 'e0ceccfa415db4c7eef667b59dec7b3db8d62e25')
+            console.error('got here is file blob tree', gitHash, outHash)
+          hashCache[gitHash] = outHash
+          var cbs = waitingForHash[gitHash]
+          if (cbs) {
+            while (cbs.length)
+              cbs.pop()(null, outHash)
+            delete waitingForHash[gitHash]
+          }
+          /*
+          if (readyToResolve) {
+            readyToResolve = false
+            console.log('resolve', waitingForHash, hashCache)
+            resolve()
+          }
+          */
         }
       })
     }
   }
 
+  function resolve() {
+    // request lookup for remaining unknown git hashes
+    // console.log('resolve', typeof waitingForHash, waitingForHash)
+    for (var gitHash in waitingForHash)
+      if (!(gitHash in hashCache)) {
+        console.error('LOOKUP!', gitHash)
+        return lookup(gitHash, function (err, hash) {
+          hashCache[gitHash] = hash
+          for (var cbs = waitingForHash[gitHash]; cbs.length; )
+            cbs.pop()(err, hash)
+          delete waitingForHash[gitHash]
+          resolve()
+        })
+      }
+    outQueue.end()
+  }
+
   function lookupCached(gitHash, cb) {
-    if (gitHash in hashCache)
-      return cb(null, hashCache[gitHash])
-    lookup(gitHash, function (err, hash) {
-      hashCache[gitHash] = hash
-      cb(err, hash)
-    })
+    var hash = hashCache[gitHash]
+    if (hash)
+      return cb(null, hash)
+    if (1) {
+      // console.log('waiting', gitHash)
+      ;(waitingForHash[gitHash] || (waitingForHash[gitHash] = [])).push(cb)
+    } else {
+      lookup(gitHash, function (err, hash) {
+        hashCache[gitHash] = hash
+        cb(err, hash)
+      })
+    }
   }
 
   function rewriteCommitOrTagFromGit(gitHasher) {
@@ -113,16 +191,18 @@ function rewriteObjectsFromGit(algorithm, lookup) {
               case 'tree':
               case 'parent':
               case 'object':
-              if (args[1] in hashCache) {
-                args.push(hashCache[args[1]].toString('hex'))
-                lines[i] = args.join(' ')
-              } else {
-                return lookupCached(args[1], function (err, hash) {
+                var hash = hashCache[args[1]]
+                if (hash) {
                   args.push(hash.toString('hex'))
                   lines[i] = args.join(' ')
-                  processLines(i+1)
-                })
-              }
+                } else {
+                  return lookupCached(args[1], function (err, hash) {
+                    if (err) return cb(err)
+                    args.push(hash.toString('hex'))
+                    lines[i] = args.join(' ')
+                    processLines(i+1)
+                  })
+                }
             }
           }
 
@@ -132,28 +212,53 @@ function rewriteObjectsFromGit(algorithm, lookup) {
     }
   }
 
-  function rewriteTreeFromGit() {
+  // Trees, commits and tags are buffered completely because they may refer to
+  // objects which are not available yet. The tree's output should not block
+  // because then the sink may not read the next object. Only blobs get passed
+  // through without buffering (or rewriting), since they don't point to any
+  // other objects.
+  function rewriteTreeFromGit(hasherIn) {
     return function (read) {
-      var b = buffered(read)
-      var readFileInfo = b.delimited(0)
-      var readGitHash = b.chunks(20)
+      var ended, bufsOut = []
 
-      return function readEntry(abort, cb) {
-        readFileInfo(abort, function (end, fileInfo) {
-          if (end) return cb(end)
-          readGitHash(abort, function (end, gitHash) {
-            if (end) return cb(end)
-            lookupCached(gitHash.toString('hex'), function (err, hash) {
-              if (err) return cb(err)
-              cb(null, Buffer.concat([
-                new Buffer(fileInfo),
-                new Buffer([0]),
-                gitHash,
-                hash
-              ]))
-            })
+      return function (abort, cb) {
+        if (ended) return cb(ended)
+        pull.collect(function (err, bufs) {
+          if (err) return cb(ended = err)
+          processBuf(Buffer.concat(bufs), 0)
+        })(read)
+
+        function processBuf(buf, i) {
+          if (i >= buf.length) {
+
+          if (hasherIn.digest.toString('hex') ==
+            'e0ceccfa415db4c7eef667b59dec7b3db8d62e25')
+            console.error('done is file tree', bufsOut.length)
+
+            ended = true
+            cb(null, Buffer.concat(bufsOut))
+            return
+          }
+
+          var j = buf.indexOf(0, i, 'ascii')
+          // pass through file info, null byte, and git hash
+          bufsOut.push(buf.slice(i, j + 21))
+          var gitHash = buf.slice(j + 1, j + 21)
+
+          if (hasherIn.digest.toString('hex') ==
+            'e0ceccfa415db4c7eef667b59dec7b3db8d62e25')
+            console.error('here is file tree', gitHash.toString('hex'))
+
+          lookupCached(gitHash.toString('hex'), function (err, hash) {
+          if (hasherIn.digest.toString('hex') ==
+            'e0ceccfa415db4c7eef667b59dec7b3db8d62e25')
+              console.error('looked up here is file tree', hash, buf.length, j + 21)
+            if (err) return cb(err)
+            // append other hash
+            bufsOut.push(hash)
+            processBuf(buf, j + 21)
           })
-        })
+        }
       }
     }
   }
--- test/rehash.js
+++ test/rehash.js
@@ -7,6 +7,14 @@ var crypto = require('crypto')
 var objectIds = Object.keys(repo)
 var objects = objectIds.map(function (id) { return repo[id] })
 
+function compareObjects(a, b) {
+  return a.data > b.data
+}
+
+function sortObjects(objs) {
+  return objs.slice().sort(compareObjects)
+}
+
 function objectEncoding(obj) {
   return obj.type == 'tree' ? 'hex' : 'utf8'
 }
@@ -51,18 +59,19 @@ function hash(type, data, encoding) {
 }
 
 function lookup(gitHash, cb) {
+  console.error('lookup', gitHash)
   if (gitHash in repo)
     cb(null, hash('sha256', repo[gitHash].data))
   else
-    cb(new Error('hash not present'))
+    cb(new Error('hash not present: ' + gitHash))
 }
 
 
-function objectsEquals(t) {
+function objectsEquals(t, objs) {
+  objs = objs || objects
   var i = 0
   return function gotObject(obj) {
-    t.deepEquals(obj, objects[i],
-      'got ' + objects[i].type + ' ' + objectIds[i])
+    t.deepEquals(obj, objs[i], 'got ' + objs[i].type) // + ' ' + objectIds[i])
     i++
   }
 }
@@ -81,17 +90,19 @@ tape('pass through', function (t) {
 })
 
 tape('rewrite object hashes', function (t) {
-  var gotObject = objectsEquals(t)
+  var gotObject = objectsEquals(t, sortObjects(objects))
   pull(
     pull.values(objects),
     expandObjects(),
     rehash.fromGit('sha256', lookup),
     rehash.toGit('sha256', 32),
     flattenObjects(),
-    pull.drain(function (obj) {
-      gotObject(obj)
-    }, function (err) {
+    pull.collect(function (err, objs) {
       t.error(err, 'rewrite and flatten objects')
+      // sortObjects(objects).forEach(gotObject)
+      t.equals(objs.length, objects.length)
+      // t.deepEquals(sortObjects(objs), )
+      // console.error(objs)
       t.end()
     })
   )
